9d139502a776ca282ae31f3a7e7b829989691dbd
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-07-20 17:24:02 +0200

    feat: initial commit

---
+++ src/test/java/com/n1b3lung0/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1b3lung0.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
